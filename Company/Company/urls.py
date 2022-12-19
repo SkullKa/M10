68564f9c15be930daeca54fddbd35aa5f197335a
SkullKa <Umbrella.1983@mail.ru>
2022-12-20 00:05:06 +0500

    'modul10.'

--- Company/Company/urls.py
+++ Company/Company/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -25,4 +25,4 @@ urlpatterns = [
     path('users', include('users.urls')),
     path('main', include('main.urls')),
     path('users/', include('django.contrib.auth.urls')),
-    path('grappelli/', include('grappelli.urls')),]+static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    ]
